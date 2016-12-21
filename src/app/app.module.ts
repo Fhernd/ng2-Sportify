4414ddb54ae038a16fbef35c047054878d14df80
John Ortiz Ordoñez <johnortizo@outlook.com>
2016-12-21 17:01:17 +0000

    Sportify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,61 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {
+  RouterModule, 
+  Routes
+} from '@angular/router';
+import {
+  LocationStrategy, 
+  HashLocationStrategy, 
+  APP_BASE_HREF
+} from '@angular/common';
 
 import { AppComponent } from './app.component';
+import {
+  AlbumComponent
+} from './components/AlbumComponent';
+import {
+  ArtistComponent
+} from './components/ArtistComponent';
+import {
+  SearchComponent
+} from './components/SearchComponent';
+import {
+  TrackComponent
+} from './components/TrackComponent';
+
+import {
+  SPOTIFY_PROVIDERS
+} from './services/SpotifyService';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchComponent },
+  { path: 'artists/:id', component: ArtistComponent },
+  { path: 'tracks/:id', component: TrackComponent },
+  { path: 'albums/:id', component: AlbumComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    AlbumComponent, 
+    ArtistComponent, 
+    SearchComponent, 
+    TrackComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    SPOTIFY_PROVIDERS, 
+    {provide: APP_BASE_HREF, useValue: '/'}, 
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
